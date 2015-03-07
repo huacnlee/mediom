8120e3799ee2b05e49c6547fee0ea9f7d32e8ac0
Jason Lee <huacnlee@gmail.com>
2015-03-07 11:02:34 +0800

    .

--- app/markdown.go
+++ app/markdown.go
@@ -10,15 +10,6 @@ var (
 	blankRegexp, _        = regexp.Compile(`>\s+<`)
 	mentionRegexp, _      = regexp.Compile(`@([\w\-\_]{3,20})`)
 	mentionFloorRegexp, _ = regexp.Compile(`#([0-9]+)楼`)
-	allowTags             = []string{
-		"p", "br", "img", "h1", "h2", "h3", "h4", "h5", "h6",
-		"blockquote", "pre", "code", "b", "i",
-		"strong", "em", "strike", "del", "u", "a", "ul",
-		"ol", "li", "span", "hr",
-	}
-	allowAttrs = []string{
-		"href", "src", "class", "title", "alt", "target", "rel", "data-floor",
-	}
 )
 
 func MarkdownGitHub(input []byte) []byte {
@@ -37,17 +28,8 @@ func MarkdownGitHub(input []byte) []byte {
 		EXTENSION_HARD_LINE_BREAK |
 		EXTENSION_NO_EMPTY_LINE_BEFORE_BLOCK
 
-	p := bluemonday.NewPolicy()
-
-	// Require URLs to be parseable by net/url.Parse and either:
-	//   mailto: http:// or https://
-	p.AllowStandardURLs()
-	p.AllowElements(allowTags...)
-	p.AllowAttrs(allowAttrs...)
-	p.AllowImages()
-
 	unsafe := Markdown(input, renderer, extensions)
-	html := p.SanitizeBytes(unsafe)
+	html := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
 	html = LinkMentionUser(html)
 	html = LinkMentionFloor(html)
 	html = RemoveBlankChars(html)
