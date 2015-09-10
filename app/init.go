4dc9ecb4db8009f91707d8b03a8ce9554a87d70e
Jason Lee <huacnlee@gmail.com>
2015-09-10 18:07:18 +0800

    集成 Qor，用于实现 admin 界面;

--- app/init.go
+++ app/init.go
@@ -5,25 +5,32 @@ import (
 	"github.com/huacnlee/mediom/app/models"
 	"github.com/huacnlee/train"
 	"github.com/revel/revel"
+	"github.com/qor/qor"
+	"github.com/qor/qor/admin"
 	"strings"
+  "net/http"
 )
+
+var Admin *admin.Admin
+var mux *http.ServeMux
 
 func init() {
 	// fmt.Println("Start app with dev mode:", revel.Config.BoolDefault("mode.dev", false))
 	// Filters is the default set of global filters.
 	revel.Filters = []revel.Filter{
-		revel.PanicFilter,             // Recover from panics and display an error page instead.
-		revel.RouterFilter,            // Use the routing table to select the right Action
+		revel.PanicFilter,						 // Recover from panics and display an error page instead.
+    AdminFilter,
+		revel.RouterFilter,						 // Use the routing table to select the right Action
 		revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
-		revel.ParamsFilter,            // Parse parameters into Controller.Params.
-		revel.SessionFilter,           // Restore and write the session cookie.
-		revel.FlashFilter,             // Restore and write the flash cookie.
-		csrf.CSRFFilter,               // CSRF
-		revel.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
-		revel.I18nFilter,              // Resolve the requested language
-		revel.InterceptorFilter,       // Run interceptors around the action.
-		revel.CompressFilter,          // Compress the result.
-		revel.ActionInvoker,           // Invoke the action.
+		revel.ParamsFilter,						 // Parse parameters into Controller.Params.
+		revel.SessionFilter,					 // Restore and write the session cookie.
+		revel.FlashFilter,						 // Restore and write the flash cookie.
+		csrf.CSRFFilter,							 // CSRF
+		revel.ValidationFilter,				 // Restore kept validation errors and save new ones from cookie.
+		revel.I18nFilter,							 // Resolve the requested language
+		revel.InterceptorFilter,			 // Run interceptors around the action.
+		revel.CompressFilter,					 // Compress the result.
+		revel.ActionInvoker,					 // Invoke the action.
 	}
 
 	train.Config.AssetsPath = "app/assets"
@@ -36,6 +43,7 @@ func init() {
 
 	revel.OnAppStart(func() {
 		models.InitDatabase()
+		initAdmin()
 
 		if revel.DevMode {
 			train.ConfigureHttpHandler(nil)
@@ -45,14 +53,39 @@ func init() {
 
 	revel.TemplateFuncs["javascript_include_tag"] = train.JavascriptTag
 	revel.TemplateFuncs["stylesheet_link_tag"] = train.StylesheetTag
-
 }
 
 var AssetsFilter = func(c *revel.Controller, fc []revel.Filter) {
-	path := c.Request.URL.Path
-	if strings.HasPrefix(path, "/assets") {
+	if strings.HasPrefix(c.Request.URL.Path, "/assets") {
 		train.ServeRequest(c.Response.Out, c.Request.Request)
 	} else {
 		fc[0](c, fc[1:])
 	}
+}
+
+var AdminFilter = func(c *revel.Controller, fc []revel.Filter) {
+	if strings.HasPrefix(c.Request.URL.Path, "/admin") {
+		mux.ServeHTTP(c.Response.Out, c.Request.Request)
+	} else {
+		fc[0](c, fc[1:])
+	}
+}
+
+func initAdmin() {
+	Admin = admin.New(&qor.Config{DB: models.DB})
+
+	Admin.AddResource(&admin.AssetManager{}, &admin.Config{Invisible: true})
+
+	topic := Admin.AddResource(&models.Topic{})
+  topic.IndexAttrs("Id", "UserId", "Title", "NodeId", "RepliesCount", "CreatedAt", "UpdatedAt")
+
+	Admin.AddResource(&models.Reply{})
+  Admin.AddResource(&models.User{})
+  Admin.AddResource(&models.Node{})
+  Admin.AddResource(&models.Notification{})
+
+
+	mux = http.NewServeMux()
+  mux.Handle("/system/", http.FileServer(http.Dir("public")))
+	Admin.MountTo("/admin", mux)
 }
